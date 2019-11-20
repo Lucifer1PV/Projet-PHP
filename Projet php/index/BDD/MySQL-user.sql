9bfdcdbbd597dbdff93a5dc8c593d05a84570c11
Lucifer1PV <44754711+Lucifer1PV@users.noreply.github.com>
2019-11-20 21:22:47 +0100

    Update MySQL-user.sql

--- Projet php/index/BDD/MySQL-user.sql
+++ Projet php/index/BDD/MySQL-user.sql
@@ -1,78 +1,168 @@
-drop database ma_base;
-drop user u_stage@localhost;
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mer 20 Novembre 2019 à 20:22
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.0.3
 
-CREATE DATABASE `ma_base` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-use `ma_base`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-CREATE USER 'u_stage'@'localhost' IDENTIFIED BY 'abcd';
-GRANT USAGE ON * . * TO 'u_stage'@'localhost' IDENTIFIED BY 'abcd' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
-GRANT ALL PRIVILEGES ON `ma_base` . * TO 'u_stage'@'localhost';
--- 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `ma_base`
+--
+
 -- --------------------------------------------------------
 
--- 
--- Table statut qui va stocker les différents statut possible (entreprise,membre,admin)un id correspondra a un statut
-CREATE TABLE IF NOT EXISTS `STATUT`(
-	ID_STATUT int(2) PRIMARY KEY,
-	NOM_STATUT varchar (255) NOT NULL
-);
--- Table des utilisateurs
+--
+-- Structure de la table `commentaire`
+--
 
-CREATE TABLE IF NOT EXISTS `UTILISATEUR` (
-  ID_USER SERIAL NOT NULL  PRIMARY KEY ,
-  UTI_PWD  varchar(255) NOT NULL,
-  UTI_MAIL varchar(255) NOT NULL,
-  UTI_NOM varchar(255) NOT NULL,
-  UTI_PRENOM varchar(255) NOT NULL,
-  ID_STATUT int(2),
-  FOREIGN KEY (`ID_STATUT`) references `STATUT`(`ID_STATUT`) on delete no action on update cascade
-);
--- Table entreprise
-CREATE TABLE IF NOT EXISTS `ENTREPRISE`(
-	ID_ENTREPRISE int(2) DEFAULT 1 PRIMARY KEY,
-	RAISON_SOCIALE varchar(255) NOT NULL,
-	TEL varchar(16) NOT NULL,
-	ID_USER BIGINT Unsigned NOT NULL  ,
-	FOREIGN KEY (ID_USER) REFERENCES UTILISATEUR(ID_USER)on delete cascade on update cascade,
-	actif boolean default false
-);
--- Table membre
-CREATE TABLE IF NOT EXISTS `MEMBRE`(
-	ID_MEMBRE int(2) PRIMARY KEY,
-	DATE_NAIS date NOT NULL,
-	TEL varchar(16) NOT NULL,
-	ID_USER BIGINT Unsigned NOT NULL,
-	FOREIGN KEY (ID_USER) REFERENCES UTILISATEUR(ID_USER)on delete cascade on update cascade,
-	DIPLOME_PREP varchar(255) NOT NULL
-);
--- Table offre de stage 
-CREATE TABLE IF NOT EXISTS `OFFRE`(
-	ID_OFFRE int(10) PRIMARY KEY,
-	TITRE varchar(255) NOT NULL,
-	DATE_DEBUT date NOT NULL,
-	MISSION varchar(255) NOT NULL,
-	CONTACT varchar(255) NOT NULL
-);
--- Table commentaire 
-CREATE TABLE IF NOT EXISTS `COMMENTAIRE`(
-	ID_USER BIGINT Unsigned NOT NULL,
-	ID_OFFRE int(10),
-    FOREIGN KEY (ID_USER) REFERENCES UTILISATEUR(ID_USER)on delete no action on update cascade,
-	FOREIGN KEY (ID_OFFRE) REFERENCES OFFRE(ID_OFFRE)on delete no action on update cascade,
-	TEXTE varchar(255) NOT NULL
-);
--- Table gestion des likes sur un articles
-CREATE TABLE IF NOT EXISTS `LIKE`(
-	ID_USER BIGINT Unsigned NOT NULL,
-	FOREIGN KEY (ID_USER) REFERENCES UTILISATEUR(ID_USER)on delete no action on update cascade,
-	ID_OFFRE int(10),
-	FOREIGN KEY (ID_OFFRE) REFERENCES OFFRE(ID_OFFRE)on delete no action on update cascade,
-	ouiLIKER boolean
-);
+CREATE TABLE `commentaire` (
+  `ID_USER` int(10) DEFAULT NULL,
+  `ID_OFFRE` int(10) DEFAULT NULL,
+  `TEXTE` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO STATUT VALUES(1,"membre");
-INSERT INTO STATUT VALUES(2,"entreprise");
-INSERT INTO STATUT VALUES(3,"admin");
+-- --------------------------------------------------------
 
+--
+-- Structure de la table `entreprise`
+--
 
-insert into  utilisateur  values(1,sha1('abcd'),'johan.bosquet@etu.univ-amu.fr','admin','..',3);
+CREATE TABLE `entreprise` (
+  `ID_ENTREPRISE` int(2) NOT NULL DEFAULT '1',
+  `RAISON_SOCIALE` varchar(255) NOT NULL,
+  `TEL` varchar(16) NOT NULL,
+  `ID_USER` int(10) DEFAULT NULL,
+  `actif` tinyint(1) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membre`
+--
+
+CREATE TABLE `membre` (
+  `ID_MEMBRE` int(2) NOT NULL,
+  `DATE_NAIS` date NOT NULL,
+  `TEL` varchar(16) NOT NULL,
+  `ID_USER` int(10) DEFAULT NULL,
+  `DIPLOME_PREP` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offre`
+--
+
+CREATE TABLE `offre` (
+  `ID_OFFRE` int(10) NOT NULL,
+  `TITRE` varchar(255) NOT NULL,
+  `DATE_DEBUT` date NOT NULL,
+  `MISSION` varchar(255) NOT NULL,
+  `CONTACT` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `ID_USER` int(10) NOT NULL,
+  `UTI_PWD` varchar(255) NOT NULL,
+  `UTI_MAIL` varchar(255) NOT NULL,
+  `UTI_NOM` varchar(255) NOT NULL,
+  `UTI_PRENOM` varchar(255) NOT NULL,
+  `ID_STATUT` int(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`ID_USER`, `UTI_PWD`, `UTI_MAIL`, `UTI_NOM`, `UTI_PRENOM`, `ID_STATUT`) VALUES
+(1, '81fe8bfe87576c3ecb22426f8e57847382917acf', 'johan.bosquet@etu.univ-amu.fr', 'admin', '..', 3);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD KEY `ID_USER` (`ID_USER`),
+  ADD KEY `ID_OFFRE` (`ID_OFFRE`);
+
+--
+-- Index pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`ID_ENTREPRISE`),
+  ADD KEY `ID_USER` (`ID_USER`);
+
+--
+-- Index pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD PRIMARY KEY (`ID_MEMBRE`),
+  ADD KEY `ID_USER` (`ID_USER`);
+
+--
+-- Index pour la table `offre`
+--
+ALTER TABLE `offre`
+  ADD PRIMARY KEY (`ID_OFFRE`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`ID_USER`),
+  ADD KEY `ID_STATUT` (`ID_STATUT`);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `commentaire_ibfk_1` FOREIGN KEY (`ID_USER`) REFERENCES `utilisateur` (`ID_USER`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `commentaire_ibfk_2` FOREIGN KEY (`ID_OFFRE`) REFERENCES `offre` (`ID_OFFRE`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD CONSTRAINT `entreprise_ibfk_1` FOREIGN KEY (`ID_USER`) REFERENCES `utilisateur` (`ID_USER`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `membre`
+--
+ALTER TABLE `membre`
+  ADD CONSTRAINT `membre_ibfk_1` FOREIGN KEY (`ID_USER`) REFERENCES `utilisateur` (`ID_USER`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`ID_STATUT`) REFERENCES `statut` (`ID_STATUT`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
